0dde57b33a13889de8491c6f87b5780d23038327
iBalll <cordeno82@gmail.com>
2023-11-21 11:33:56 +0700

    update db_notes.sql

--- html/db_notes.sql
+++ html/db_notes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Nov 2023 pada 20.40
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Generation Time: Nov 21, 2023 at 05:32 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `notes`
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(12) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `pekerjaan` varchar(50) DEFAULT NULL,
+  `nomor_telpon` varchar(20) DEFAULT NULL,
+  `lokasi` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `id_user`, `foto`, `nama`, `email`, `pekerjaan`, `nomor_telpon`, `lokasi`) VALUES
+(14, 5, 'photo-contact/WIN_20231114_02_56_03_Pro.jpg', 'Iqbal Suranata', 'iqbalsuranata303@gmail.com', 'Software Engineer', '085156427733', 'Banjarnegara'),
+(15, 7, 'photo-contact/love.jpeg', 'Iqbal Suranata', 'iqbalsuranata303@gmail.com', 'Software Engineer', '085156427733', 'Banjarnegara');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
 --
 
 CREATE TABLE `notes` (
@@ -33,67 +58,151 @@ CREATE TABLE `notes` (
   `konten` text NOT NULL,
   `tanggal` datetime NOT NULL DEFAULT current_timestamp(),
   `favorite` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `notes`
---
-
-INSERT INTO `notes` (`id`, `judul`, `konten`, `tanggal`, `favorite`) VALUES
-(1, 'Percobaan 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '2023-11-15 02:31:43', 0),
-(2, 'Percobaan 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ', '2023-11-15 02:33:54', 1),
-(3, 'Percobaan 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2023-11-15 02:34:22', 0);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tags`
+-- Table structure for table `tags`
 --
 
 CREATE TABLE `tags` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data untuk tabel `tags`
+-- Table structure for table `todo`
 --
 
-INSERT INTO `tags` (`id`, `nama`) VALUES
-(1, 'Pemweb'),
-(2, 'Kuliah');
+CREATE TABLE `todo` (
+  `id` int(12) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `deadline` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `todo`
+--
+
+INSERT INTO `todo` (`id`, `id_user`, `judul`, `deskripsi`, `deadline`) VALUES
+(84, 5, 'confess ke Vania Nur Aini', 'semoga diterima, amiin yaa Rabbal alamiin...\r\nBismillah Yaa Allah', '2023-11-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `alamat` varchar(512) NOT NULL,
+  `pekerjaan` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(13) NOT NULL,
+  `password` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `foto`, `username`, `alamat`, `pekerjaan`, `email`, `no_hp`, `password`) VALUES
+(5, 'photo-profile/default.jpg', 'Iqbal Suranata', 'Jalan Raya Lengkong', 'Software Engineer', 'suranata@gmail.com', '085156427733', '81dc9bdb52d04dc20036dbd8313ed055'),
+(6, 'photo-profile/default.jpg', 'LickMyBall', '-', '-', 'iqbalsuranata303@gmail.com', '-', '81dc9bdb52d04dc20036dbd8313ed055'),
+(7, 'photo-profile/love2.jpeg', 'Nataaa', 'Jalan Raya Lengkong', 'Software Engineer', 'cordeno82@gmail.com', '0851564277333', '81dc9bdb52d04dc20036dbd8313ed055');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `notes`
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `notes`
 --
 ALTER TABLE `notes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tags`
+-- Indexes for table `tags`
 --
 ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `notes`
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `tags`
+-- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `todo`
+--
+ALTER TABLE `todo`
+  ADD CONSTRAINT `todo_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
